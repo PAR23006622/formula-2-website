1e815e15603d00b43e66a27ebf7ed94fbbecc70e
PAR23006622 <par23006622@heartofyorkshire.ac.uk>
2024-11-15 13:25:02 +0000

    v0.5.12

--- pages/driverCharts.tsx
+++ pages/driverCharts.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Sidebar from '../components/Sidebar';
 import DriverChartComponent from '../components/DriverChartComponent';
 import styles from './charts.module.css';
@@ -8,12 +8,12 @@ import styles from './charts.module.css';
 const ChartsPage: React.FC = () => {
   return (
     <div className={styles.global}>
-    <Analytics /> <SpeedInsights/>
-    <div className={styles.container}>
-      <Sidebar />
+      <Analytics /> <SpeedInsights/>
       <div className={styles.container}>
-        <DriverChartComponent />
-      </div>
+        <Sidebar />
+          <div className={styles.container}>
+            <DriverChartComponent />
+          </div>
       </div>
     </div>
   );
